a7b41b1935dcf6de701c35cbc473578b90ac28b2
Jeremy Miller <jeremy.j.miller@cerner.com>
2016-09-08 00:06:03 -0500

    adding supported platforms

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,11 @@ version          '0.2.0'
 issues_url 'https://github.com/som3guy/sickrage/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/som3guy/sickrage.git' if respond_to?(:source_url)
 
+supports 'centos', '>= 6.7'
+supports 'fedora', '>= 6.7'
+supports 'redhat', '>= 6.7'
+supports 'scientific', '>= 6.7'
+
 depends 'git', '~> 5.0.0'
 depends 'build-essential', '~> 6.0.4'
 depends 'yum-epel', '~> 1.0.0'
